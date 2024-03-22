c8abb4ba43c7e9a070dea9ada15680fd6f101065
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-03-22 14:06:57 +0000

    codigo analisado e verificado

---
+++ RandomTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RandomTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r 1 = new Random ( 2 2 ) ;
+            Random r 2 = new Random ( 2 2 ) ;
+            for (int i = 0 ; i < 4 ; i ++)
+                Console.WriteLine( r1.Next());
+            Console.WriteLine();
+            for (int i = 0 ; i < 4 ; i ++)
+                Console.WriteLine ( r2.Next ());
+        }
+    }
+}
