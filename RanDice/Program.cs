75158c994545a606dfa80393dab5a9de8c55a2c7
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-03-22 14:25:14 +0000

    foi criado o programa

---
+++ RanDice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+
+            int a = int.Parse(args[0]);
+            
+            int b = int.Parse(args[1]);
+            
+            Random rnd = new Random(b);
+                for (int i = 0; i <a; i++) 
+                {
+                int valor = rnd.Next(1,7);
+                soma += valor;
+                }
+                
+            Console.WriteLine(soma);
+        }
+    }
+}
